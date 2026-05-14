9193bd992c51bfb2a75043e0116710e301ada555
AME45 <2231488391@qq.com>
2026-05-14 11:32:03 +0800

    菜品分类分页查询，按名字类型查询，添加，修改，启用禁用的代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -0,0 +1,75 @@
+package com.sky.controller.admin;
+
+
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/category")
+@Slf4j
+@Api("菜品分类相关接口")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 分页查询
+     *
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("分类分页查询")
+    public Result<PageResult> page(CategoryPageQueryDTO categoryPageQueryDTO){
+        PageResult pageResult = categoryService.pageQuery(categoryPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+
+    /**
+     * 新增菜品种类
+     *
+     * @param categoryDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增菜品分类")
+    public Result addCategory(@RequestBody CategoryDTO categoryDTO){
+        categoryService.addCategory(categoryDTO);
+        return Result.success();
+    }
+
+    /**
+     * 修改菜品种类
+     *
+     * @param categoryDTO
+     * @return
+     */
+    @PutMapping
+    @ApiOperation("修改菜品分类")
+    public Result updateCategory(@RequestBody CategoryDTO categoryDTO){
+        categoryService.update(categoryDTO);
+        return Result.success();
+    }
+
+    /**
+     * 启用和禁用菜品种类
+     *
+     * @param
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启动和禁用菜品分类")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        categoryService.startOrStop(status,id);
+        return Result.success();
+    }
+}
