19e819beeee87e8934eacf9ede59d1f6b97b6879
AME45 <2231488391@qq.com>
2026-05-19 16:06:04 +0800

    菜品分页查询代码的开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -2,17 +2,16 @@ package com.sky.controller.admin;
 
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -34,5 +33,12 @@ public class DishController {
     public Result addDish(@RequestBody DishDTO dishDTO) {
         dishService.addDishwithFlavor(dishDTO);
         return Result.success();
+    }
+
+    @GetMapping("/page")
+    @ApiOperation("分页查询")
+    public Result<PageResult> pageDish(DishPageQueryDTO dishPageQueryDTO) {
+        PageResult pageResult = dishService.page(dishPageQueryDTO);
+        return Result.success(pageResult);
     }
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -2,8 +2,12 @@ package com.sky.service;
 
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
+import com.sky.result.PageResult;
 
 public interface DishService {
     void addDishwithFlavor(DishDTO dishDTO);
+
+    PageResult page(DishPageQueryDTO dishPageQueryDTO);
 }
